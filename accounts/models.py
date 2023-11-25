e4bc2ebb1a83cc9d4a9299bf845aa6e4296cd244
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-11-25 22:27:01 +0330

    edit custom user and manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,31 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,username, email, password=None):
+    def create_user(self,username, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password):
-        user = self.create_user(username, email, password)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50, unique=True)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects=UserManager()
 
