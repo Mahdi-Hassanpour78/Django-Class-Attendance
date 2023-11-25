6030ca4797f3e8b7fcf967533406371075fd4925
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-11-25 22:04:24 +0330

    add admin.pyfor accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active']
+    search_fields = ['username', 'email']
+
+
+admin.site.register(User, CustomUserAdmin)
