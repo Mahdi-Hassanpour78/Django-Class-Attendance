15c0228772c28dc23bebc16c971a1bf9d7df3218
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-11-25 18:15:35 +0330

    add registration view in accounts\views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializers
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializers
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
