801761f68706039cda3f9a0cfdf123e359d86d83
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-11-25 21:59:20 +0330

    add login view for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSerializers
+from .serializers import UserSerializers, LoginSerializer
+from rest_framework.authtoken.models import Token
 
 
 class RegisterView(generics.CreateAPIView):
@@ -12,3 +13,14 @@ class RegisterView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
