114e0a95d0db0b518cd01662b52bc14d65965334
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-11-25 23:59:39 +0330

    edit registrationview becuase it doesnt work

--- accounts/views.py
+++ accounts/views.py
@@ -3,16 +3,20 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer, LoginSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class LoginView(generics.CreateAPIView):
