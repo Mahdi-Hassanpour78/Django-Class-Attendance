8decc9dbd2557ba14f838c67c3a4c316fd5d9350
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-11-26 17:18:13 +0330

    add semester and class model in classes/models.py

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Semester(models.Model):
+    name = models.CharField(max_length=10)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=10, unique=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    professor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
